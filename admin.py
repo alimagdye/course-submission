111fcfd40cbfcd46850e9acc7e2552bf0d4697cd
Ali Magdy <alimagdye1@gmail.com>
2026-02-09 01:17:08 +0200

    Add admin configuration for Course, Lesson, Question, Choice, and Submission models

---
+++ admin.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+from .models import Course, Lesson, Enrollment, Question, Choice, Submission
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+
+class QuestionInline(admin.TabularInline):
+    model = Question
+    extra = 1
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ('text', 'lesson', 'grade')
+    search_fields = ('text',)
+
+
+class LessonAdmin(admin.ModelAdmin):
+    inlines = [QuestionInline]
+    list_display = ('title', 'course')
+    search_fields = ('title',)
+
+
+admin.site.register(Course)
+admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Enrollment)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)
